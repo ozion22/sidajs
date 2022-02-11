09da63d6bc5b6174ba09f625dd82790d1285c003
ozion22 <94684892+ozion22@users.noreply.github.com>
2022-02-11 12:00:03 +0100

    Add files via upload

--- domjs.js
+++ domjs.js
@@ -18,3 +18,22 @@ function showDiv(n) {
     }
     imageArray[imageCounter - 1].style.display = "block";
 }
+
+var acc = document.getElementsByClassName("accordion");
+var a;
+
+for (a = 0; a < acc.length; a++) {
+  acc[a].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
